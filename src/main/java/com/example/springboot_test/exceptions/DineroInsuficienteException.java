91ce24a82ec5ea60397153cb7f85b23b05080e8f
Gustavo Caradonna <gustavocaradonna@gmail.com>
2024-11-17 14:24:51 -0300

    creando clases

---
+++ src/main/java/com/example/springboot_test/exceptions/DineroInsuficienteException.java
@@ -0,0 +1,8 @@
+package com.example.springboot_test.exceptions;
+
+public class DineroInsuficienteException extends RuntimeException{
+
+    public DineroInsuficienteException(String message) {
+        super(message);
+    }
+}
